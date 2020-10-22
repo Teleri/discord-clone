5812f82b455d4aab9e3e83cf3185a00160424056
Michael Knott <knottweb@gmail.com>
2020-10-22 17:22:47 +0100

    import material ui and use icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
   return (
     <div>
       <h2>I am the Sidebar</h2>
+      <div class="sidebar__top">
+        <h3>Home</h3>
+        <ExpandMoreIcon />
+      </div>
     </div>
   );
 }
